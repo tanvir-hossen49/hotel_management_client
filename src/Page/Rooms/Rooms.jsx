3b6ae0824aad19696e44969eac24adf14e909b80
miasumon <miasumon.ru@gmail.com>
2024-11-15 11:58:48 +0600

    daisyUI installed and simple card added

--- src/Page/Rooms/Rooms.jsx
+++ src/Page/Rooms/Rooms.jsx
@@ -1,7 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
 
 const Rooms = () => {
-  return <div>This is rooms page</div>;
+
+
+  const [apartments, setApartments] = useState([])
+  
+
+  useEffect(()=>{
+    fetch('rooms.json')
+    .then((res)=> res.json())
+    .then((data)=> {
+      setApartments(data)
+  
+    })
+
+  },[])
+
+ console.log(apartments)
+
+  return (
+    <div>
+       {
+        apartments.map((apartment, index)=> 
+          <div key={index} className="card card-compact bg-base-100 w-96 shadow-xl">
+        <figure>
+          <img
+            src={apartment.apartment_image}
+            alt="Shoes" />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">Shoes!</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+      </div>
+            
+        
+            
+        )
+       }
+    </div>
+  );
 };
 
 export default Rooms;
